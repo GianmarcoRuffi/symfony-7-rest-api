7416d366773aa7fe8db43f80fb4248ec4418b5e8
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2024-03-24 17:03:26 +0100

    staging-2: code cleaning

--- src/Service/BikeService.php
+++ src/Service/BikeService.php
@@ -6,7 +6,6 @@ use App\Entity\Bike;
 use App\Entity\Engine;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -49,32 +48,6 @@ class BikeService
         }
     }
 
-
-    // public function getAllBikes(): JsonResponse
-    // {
-    //     try {
-    //         $bikes = $this->entityManager->getRepository(Bike::class)->findAll();
-
-    //         $data = [];
-    //         foreach ($bikes as $bike) {
-    //             $data[] = [
-    //                 'id' => $bike->getId(),
-    //                 'brand' => $bike->getBrand(),
-    //                 'engine' => [
-    //                     'name' => $bike->getEngine()->getName(),
-    //                     'serial_code' => $bike->getEngine()->getSerialCode(),
-    //                     'manufacturer' => $bike->getEngine()->getManufacturer(),
-    //                     'horsepower' => $bike->getEngine()->getHorsepower(),
-    //                 ],
-    //                 'color' => $bike->getColor(),
-    //             ];
-    //         }
-
-    //         return new JsonResponse($data);
-    //     } catch (\Exception $e) {
-    //         return new JsonResponse(['error' => $e->getMessage()], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
-    //     }
-    // }
 
     public function createBike(): JsonResponse
     {
@@ -120,33 +93,6 @@ class BikeService
 
         return new JsonResponse($data);
     }
-
-    // public function getBikeById(int $id): ?JsonResponse
-    // {
-    //     $bike = $this->entityManager->getRepository(Bike::class)->find($id);
-
-    //     // if (!$bike) {
-    //     //     return new JsonResponse('No bike found for id: ' . $id, JsonResponse::HTTP_NOT_FOUND);
-    //     // }
-
-    //     if (!$bike) {
-    //         return ['error' => 'No bike found for id: ' . $id];
-    //     }
-
-    //     $data = [
-    //         'id' => $bike->getId(),
-    //         'brand' => $bike->getBrand(),
-    //         'engine' => [
-    //             'name' => $bike->getEngine()->getName(),
-    //             'serial_code' => $bike->getEngine()->getSerialCode(),
-    //             'manufacturer' => $bike->getEngine()->getManufacturer(),
-    //             'horsepower' => $bike->getEngine()->getHorsepower(),
-    //         ],
-    //         'color' => $bike->getColor(),
-    //     ];
-
-    //     return new JsonResponse($data);
-    // }
 
 
     public function getBikeById(int $id): ?array
